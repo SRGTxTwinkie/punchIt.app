e0d3eba17a63aa7526186c28470f643443fcb080
SRGTxTwinkie <zandreisbig@yahoo.com>
2021-11-28 14:44:05 -0500

    style changes

--- src/footer.js
+++ src/footer.js
@@ -9,14 +9,14 @@ function Footer() {
                     <img
                         src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
                         alt="Github"
-                        width="50px" 
-                        style={{opacity: "0.3"}}/>
+                        width="50px"
+                        style={{opacity: "0.3", borderRadius:"5px", marginRight: "5px"}}/>
                 </a>
                 <a href="https://www.linkedin.com/in/zane-reisbig-328119200/" target="_blank" rel="noopener noreferrer">
                     <img
                         src={logo}
                         alt="LinkedIn"
-                        width="50px"
+                        width="58px"
                         style={{opacity: "0.3"}}/>
                 </a>
                 <h5>Created by Zane Reisbig <span style={{opacity:"0.2"}}> with love </span></h5> 
